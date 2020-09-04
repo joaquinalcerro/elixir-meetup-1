eaedc279769e32288590c8fe9da1bc03a99e03de
Joaquin Alcerro <joaquin@nimbbo.com>
2020-09-04 13:45:09 -0600

    Updates for meetup 2

--- meetup1/weather/lib/weather/parallel.ex
+++ meetup1/weather/lib/weather/parallel.ex
@@ -1,21 +1,27 @@
 defmodule Weather.Parallel do
-
   def pmap(collection, fun) do
     collection
-    |> Enum.map(&new_process(&1, self, fun))
+    |> Enum.map(&new_process(&1, self(), fun))
     |> Enum.map(&print/1)
   end
 
-  def new_process(item, parent, fun) do
-    spawn_link fn ->
-      send parent, {self, fun.(item)}
-    end
+  def pmap2(collection, module, fun) do
+    collection
+    |> Enum.map(&Task.async(module, fun, [&1]))
+    |> Enum.map(&Task.await/1)
   end
 
-  def print(item) do
+  ## Funciones privadas
+
+  defp new_process(item, parent, fun) do
+    spawn_link(fn ->
+      send(parent, {self(), fun.(item)})
+    end)
+  end
+
+  defp print(item) do
     receive do
       {^item, result} -> result
     end
   end
-
 end
--- meetup1/weather/lib/weather/worker.ex
+++ meetup1/weather/lib/weather/worker.ex
@@ -1,42 +1,40 @@
 defmodule Weather.Worker do
-
   def temperature_of(location) do
     weather_data =
       url_for(location)
-      |> HTTPoison.get
+      |> HTTPoison.get()
       |> parse_response
 
     case weather_data do
       {:ok, temp} -> "#{location}: #{temp} Celcius"
-      :error      -> "#{location}: Not Found"
+      :error -> "#{location}: Not Found"
     end
   end
 
-  def url_for(location) do
+  defp url_for(location) do
     location = URI.encode(location)
     "http://api.openweathermap.org/data/2.5/weather?q=#{location}&appid=#{apikey()}"
   end
 
-  def parse_response({:ok, %HTTPoison.Response{ body: body, status_code: 200 }}) do
+  defp parse_response({:ok, %HTTPoison.Response{body: body, status_code: 200}}) do
     body
-    |> JSON.decode!
+    |> JSON.decode!()
     |> compute_temperature
   end
 
-  def parse_response(_) do
+  defp parse_response(_) do
     :error
   end
 
-  def compute_temperature(json) do
+  defp compute_temperature(json) do
     temp =
       (json["main"]["temp"] - 273.15)
       |> Float.round(1)
+
     {:ok, temp}
   end
 
-  def apikey do
-    "COLOCA TU API KEY DENTRO DE ESTAS COMILLAS"
+  defp apikey do
+    "d173d4b2bce2f469fe6d6b3c39f2b101"
   end
-
-  
 end
